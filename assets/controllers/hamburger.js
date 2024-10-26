7a4729edc68507cd2632586325db92d9f5d7cfe4
Angelina Besson <formadeltiempoange@gmail.com>
2024-10-25 22:42:37 -0300

    menu hamburguesa con js

---
+++ assets/controllers/hamburger.js
@@ -0,0 +1,26 @@
+
+const headerBg = document.querySelector(".header");
+const panelIsActive = document.querySelector(".menu-list");
+
+
+export default function menuHamburger(btnHamburger,panel,menuLink){
+    document.addEventListener("click",(e)=>{
+    if(e.target.matches(btnHamburger)||e.target.matches(`${btnHamburger} *`)){
+        document.querySelector(panel).classList.toggle("is-active");
+        document.querySelector(btnHamburger).classList.toggle("is-active");
+        if(e.target.matches(menuLink)){
+            document.querySelector(panel).classList.remove("is-active");
+            document.querySelector(btnHamburger).classList.remove("is-active");
+        }
+        if(panelIsActive.classList.contains("is-active")){
+            headerBg.classList.remove("blur")
+        }else{
+            headerBg.classList.add("blur")
+        }
+        ;
+    }
+    })
+
+}
+
+menuHamburger(".hamburger",".panel",".menu-link");
